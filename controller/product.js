a45f37e47cdb9eb2bf50b71be269e6dffc23352e
imtiyaj786 <imtiyaj1523@gmail.com>
2023-08-13 18:20:04 +0530

    server side rendring concept

--- controller/product.js
+++ controller/product.js
@@ -2,6 +2,32 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const model = require("../models/Product");
 const Product = model.Product;
+const ejs = require("ejs");
+const path = require("path");
+
+// views   w
+// SSR - Server side render
+exports.getAllProductsSSR = async (req, res) => {
+  const products = await Product.find();
+  ejs.renderFile(
+    path.resolve(__dirname, "../views/index.ejs"),
+    { products: products },
+    function (err, str) {
+      res.send(str);
+    }
+  );
+};
+
+// using for direct html server side rendring for add.ejs file
+exports.getAddForm = async (req, res) => {
+  const products = await Product.find();
+  ejs.renderFile(
+    path.resolve(__dirname, "../views/add.ejs"),
+    function (err, str) {
+      res.send(str);
+    }
+  );
+};
 
 // Create model
 // note - only hen i create a model that We have to Write Instent NeW keyWords.
